bb55459d6656e6d9fce94cb74a63593b3e858d05
dreamonovich <dreamonovich@gmail.com>
2023-11-18 11:46:16 +0300

    Added half-stroke and win-lighting

---
+++ src/WinWindow.py
@@ -0,0 +1,17 @@
+from ui.ui_WinWindow import Ui_MainWindow
+from PyQt5.QtWidgets import QMainWindow
+import themes_config
+
+class WinWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def change_theme(self, theme):
+        self.setStyleSheet(
+            "QWidget {" + f'color: {themes_config.themes[theme]["text_color"]}; background-color: {themes_config.themes[theme]["background_color"]};' + "}"
+            "QPushButton {\n"
+                            f"   background-color: {themes_config.themes[theme]['button_color']};\n"
+                            "    border: none;\n"
+                            "    border-radius: 10px;\n"
+                            "}\n")
